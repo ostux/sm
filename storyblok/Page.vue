72f232b459532dce99467db08c9c8f8c17dee43e
Andras Toth <andras@andrastoth.eu>
2023-03-22 18:39:58 +0000

    Add storyblok

---
+++ storyblok/Page.vue
@@ -0,0 +1,12 @@
+<template>
+  <div v-editable="blok"
+       class="px-4">
+    <StoryblokComponent v-for="blok in blok.body"
+                        :key="blok._uid"
+                        :blok="blok" />
+  </div>
+</template>
+ 
+<script setup lang="ts">
+  defineProps({ blok: Object });
+</script>
